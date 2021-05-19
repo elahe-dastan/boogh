a623cb2b1afb7a594d7b4a6375b8b9986fe52607
Elahe Dastan <elahe.dstn@gmail.com>
2021-05-20 01:00:58 +0430

    run custom file

--- main.py
+++ main.py
@@ -25,9 +25,12 @@ from models.tester import model_test
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--n_route', type=int, default=228)
-parser.add_argument('--n_his', type=int, default=12)
-parser.add_argument('--n_pred', type=int, default=9)
+# parser.add_argument('--n_route', type=int, default=228)
+parser.add_argument('--n_route', type=int, default=2)
+# parser.add_argument('--n_his', type=int, default=12)
+parser.add_argument('--n_his', type=int, default=3)
+# parser.add_argument('--n_pred', type=int, default=9)
+parser.add_argument('--n_pred', type=int, default=1)
 parser.add_argument('--batch_size', type=int, default=50)
 parser.add_argument('--epoch', type=int, default=50)
 parser.add_argument('--save', type=int, default=10)
@@ -48,7 +51,8 @@ blocks = [[1, 32, 64], [64, 32, 128]]
 
 # Load wighted adjacency matrix W
 if args.graph == 'default':
-    W = weight_matrix(pjoin('./dataset', f'PeMSD7_W_{n}.csv'))
+    # W = weight_matrix(pjoin('./dataset', f'PeMSD7_W_{n}.csv'))
+    W = weight_matrix(pjoin('./dataset', f't_W_{n}.csv'))
 else:
     # load customized graph weight matrix
     W = weight_matrix(pjoin('./dataset', args.graph))
@@ -60,9 +64,12 @@ Lk = cheb_poly_approx(L, Ks, n)
 tf.add_to_collection(name='graph_kernel', value=tf.cast(tf.constant(Lk), tf.float32))
 
 # Data Preprocessing
-data_file = f'PeMSD7_V_{n}.csv'
-n_train, n_val, n_test = 34, 5, 5
-PeMS = data_gen(pjoin('./dataset', data_file), (n_train, n_val, n_test), n, n_his + n_pred)
+data_file = f't_V_{n}.csv'
+# data_file = f'PeMSD7_V_{n}.csv'
+n_train, n_val, n_test = 1, 0, 0
+# n_train, n_val, n_test = 34, 5, 5
+PeMS = data_gen(pjoin('./dataset', data_file), (n_train, n_val, n_test), n, n_his + n_pred, 96)
+# PeMS = data_gen(pjoin('./dataset', data_file), (n_train, n_val, n_test), n, n_his + n_pred)
 print(f'>> Loading dataset with Mean: {PeMS.mean:.2f}, STD: {PeMS.std:.2f}')
 
 if __name__ == '__main__':
