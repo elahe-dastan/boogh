54e97d7b7152a61a6d55f44cb38da8f936dc079d
Elahe Dastan <elahe.dstn@gmail.com>
2021-07-01 22:21:04 +0430

    -

--- main.py
+++ main.py
@@ -25,12 +25,12 @@ from models.tester import model_test
 import argparse
 
 parser = argparse.ArgumentParser()
-# parser.add_argument('--n_route', type=int, default=228)
-parser.add_argument('--n_route', type=int, default=2)
-# parser.add_argument('--n_his', type=int, default=12)
-parser.add_argument('--n_his', type=int, default=3)
-# parser.add_argument('--n_pred', type=int, default=9)
-parser.add_argument('--n_pred', type=int, default=1)
+parser.add_argument('--n_route', type=int, default=228)
+# parser.add_argument('--n_route', type=int, default=2)
+parser.add_argument('--n_his', type=int, default=12)
+# parser.add_argument('--n_his', type=int, default=3)
+parser.add_argument('--n_pred', type=int, default=9)
+# parser.add_argument('--n_pred', type=int, default=1)
 parser.add_argument('--batch_size', type=int, default=50)
 parser.add_argument('--epoch', type=int, default=50)
 parser.add_argument('--save', type=int, default=10)
@@ -47,13 +47,13 @@ print(f'Training configs: {args}')
 n, n_his, n_pred = args.n_route, args.n_his, args.n_pred
 Ks, Kt = args.ks, args.kt
 # blocks: settings of channel size in st_conv_blocks / bottleneck design
-# blocks = [[1, 32, 64], [64, 32, 128]]
-blocks = [[1, 1, 64], [64, 32, 128]]
+blocks = [[1, 32, 64], [64, 32, 128]]
+# blocks = [[1, 1, 64], [64, 32, 128]]
 
 # Load wighted adjacency matrix W
 if args.graph == 'default':
-    # W = weight_matrix(pjoin('./dataset', f'PeMSD7_W_{n}.csv'))
-    W = weight_matrix(pjoin('./dataset', f't_W_{n}.csv'))
+    W = weight_matrix(pjoin('./dataset', f'PeMSD7_W_{n}.csv'))
+    # W = weight_matrix(pjoin('./dataset', f't_W_{n}.csv'))
 else:
     # load customized graph weight matrix
     W = weight_matrix(pjoin('./dataset', args.graph))
@@ -62,15 +62,16 @@ else:
 L = scaled_laplacian(W)
 # Alternative approximation method: 1st approx - first_approx(W, n).
 Lk = cheb_poly_approx(L, Ks, n)
+print(Lk)
 tf.add_to_collection(name='graph_kernel', value=tf.cast(tf.constant(Lk), tf.float32))
 
 # Data Preprocessing
-data_file = f't_V_{n}.csv'
-# data_file = f'PeMSD7_V_{n}.csv'
+# data_file = f't_V_{n}.csv'
+data_file = f'PeMSD7_V_{n}.csv'
 n_train, n_val, n_test = 1, 0, 0
 # n_train, n_val, n_test = 34, 5, 5
-PeMS = data_gen(pjoin('./dataset', data_file), (n_train, n_val, n_test), n, n_his + n_pred, 4)
-# PeMS = data_gen(pjoin('./dataset', data_file), (n_train, n_val, n_test), n, n_his + n_pred)
+# PeMS = data_gen(pjoin('./dataset', data_file), (n_train, n_val, n_test), n, n_his + n_pred, 4)
+PeMS = data_gen(pjoin('./dataset', data_file), (n_train, n_val, n_test), n, n_his + n_pred)
 print(f'>> Loading dataset with Mean: {PeMS.mean:.2f}, STD: {PeMS.std:.2f}')
 
 if __name__ == '__main__':
